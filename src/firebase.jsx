aedd002ef113f800222218f3d065ce1c2c795af3
Ashish11-19 <ashishpathak0719@gmail.com>
2024-12-08 16:00:15 +0530

    chnge firebase

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-    apiKey: "AIzaSyDzP-Bid_XLPptLi_v_5_QAgWCXd1xR_sU",
-    authDomain: "fitbuddy-81017.firebaseapp.com",
-    projectId: "fitbuddy-81017",
-    storageBucket: "fitbuddy-81017.firebasestorage.app",
-    messagingSenderId: "781316976668",
-    appId: "1:781316976668:web:28c0ee580816a0d398efd9",
-    measurementId: "G-WH4C6PCLG5",
-    databaseURL:"https://console.firebase.google.com/u/0/project/fitbuddy-81017/database/fitbuddy-81017-default-rtdb/data/~2F"
+  apiKey: "AIzaSyAD2Ccm6nZOsw-zi8YKgqUNpzVLiVR6GYA",
+  authDomain: "fit-buddy-b0518.firebaseapp.com",
+  projectId: "fit-buddy-b0518",
+  storageBucket: "fit-buddy-b0518.firebasestorage.app",
+  messagingSenderId: "254737679555",
+  appId: "1:254737679555:web:bb9959e8014a7a12a48e76",
+  measurementId: "G-MP5Y1MWS0T",
+   // databaseURL:"https://console.firebase.google.com/u/0/project/fitbuddy-81017/database/fitbuddy-81017-default-rtdb/data/~2F"
   };
 
   export const app = initializeApp(firebaseConfig); 
